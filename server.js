d4e63d2bad3cc9da8564ca6041e13ad1facad8d5
Abbie Bortz <abbie@Abbies-MBP.attlocal.net>
2024-02-04 14:10:20 -0500

    Added server, Axios, and ChartJS

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+const budget = {
+    myBudget: [
+    { 
+        title: 'Eat out',   
+        budget: 30
+    },          
+    { 
+        title: 'Rent',      
+        budget: 350
+    },          
+    { 
+        title: 'Groceries', 
+        budget: 90
+    },          
+    { 
+        title: 'Utilities', 
+        budget: 150
+    },          
+    { 
+        title: 'Transport', 
+        budget: 50
+    },          
+    { 
+        title: 'Entertainment', 
+        budget: 100
+    }
+    ]
+};
+
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/budget', (req, res) => {
+    res.json(budget);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
